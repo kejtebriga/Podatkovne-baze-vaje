05d9bbb3fd7169abcc3b700c4c79618427ac919f
kejtebriga <kej.te.briga0@gmail.com>
2025-10-16 10:00:56 +0200

    dodala zapiske

--- izpisi_file.sql
+++ izpisi_file.sql
@@ -1,2 +1,31 @@
-SELECT * 
+SELECT naslov, dolzina, ocena
 FROM film
+ORDER BY ocena DESC;
+
+
+SELECT naslov, dolzina, ocena
+FROM film
+WHERE dolzina BETWEEN 120 AND 150
+ORDER BY ocena DESC
+LIMIT 20;
+
+
+SELECT naslov, dolzina, ocena
+FROM film
+WHERE dolzina >= 120 AND dolzina <= 150
+ORDER BY ocena DESC;
+
+
+
+SELECT naslov, dolzina, ocena
+FROM film
+WHERE dolzina BETWEEN 120 AND 150
+ORDER BY ocena DESC, dolzina -- padajoče po oceni, če majo isto oceno jih po dolžini uredi
+LIMIT 20;
+
+
+SELECT naslov, glasovi/dolzina AS glasovi_dolzina, ocena
+FROM film
+WHERE dolzina BETWEEN 120 AND 150
+ORDER BY glasovi_dolzina;
+
